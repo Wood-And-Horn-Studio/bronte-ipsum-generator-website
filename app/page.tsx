b7f9404aeb2a7973ee08975092b4ce310511551d
Aman Bhogal <asbhogal1@live.co.uk>
2025-06-20 22:38:42 +0100

    feat: add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import GenerateField from "@/src/components/GenerateField";
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="max-w-[1920px] w-full mx-auto">
+    <main className="max-w-[1920px] w-full mx-auto justify-between md:justify-end">
       <div>
         <h1>Brontë Ipsum</h1>
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-between">
@@ -17,7 +18,39 @@ export default function Home() {
           <p>Created by <Link className="underline" href="https://woodandhorn.studio" target="_blank">Wood & Horn Studio</Link></p>
         </div>
       </div>
-      <GenerateField />
+      <div>
+        <div className="hidden md:grid md:grid-cols-3 gap-2">
+          <Image
+            className="w-full"
+            src="/Charlotte_Bronte_by_George_Richmond.jpg"
+            alt="Charlotte Brontë by George Richmond"
+            height={600}
+            width={400}
+            priority
+            loading="eager"
+          />
+          <Image
+            className="w-full"
+            src="/Charlotte_Brontë_by_Patrick_Branwell_Brontë_restored.jpg"
+            alt="Charlotte Brontë by Patrick Branwell Brontë"
+            height={600}
+            width={400}
+            priority
+            loading="eager"
+
+          />
+          <Image
+            className="w-full"
+            src="/The_Brontë_Sisters_by_Patrick_Branwell_Brontë.jpg"
+            alt="The Brontë Sisters by Patrick Branwell Brontë"
+            height={600}
+            width={400}
+            priority
+            loading="eager"
+          />
+        </div>
+        <GenerateField />
+      </div>
     </main>
   );
 }
