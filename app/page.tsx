920ce1a37b3eb5369a7999287828b1033d115575
Aman Bhogal <asbhogal1@live.co.uk>
2025-06-20 22:07:59 +0100

    feat: add bottom link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import GenerateField from "@/src/components/GenerateField";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: 'Brontë Ipsum | An Alternative Lorem Ipsum Generator',
@@ -11,7 +12,10 @@ export default function Home() {
     <main className="max-w-[1920px] w-full mx-auto">
       <div>
         <h1>Brontë Ipsum</h1>
-        <p className="text-lg mb-6">A Lorem Ipsum alternative generator that uses text from the works of the Brontë sisters.</p>
+        <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-between">
+          <p className="text-lg">A Lorem Ipsum alternative generator that uses text from the works of the Brontë sisters.</p>
+          <p>Created by <Link className="underline" href="https://woodandhorn.studio" target="_blank">Wood & Horn Studio</Link></p>
+        </div>
       </div>
       <GenerateField />
     </main>
